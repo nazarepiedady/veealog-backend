0922536f57f50f0831a642861781c327913d9888
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-05 21:43:05 +0100

    chore: create the model `profile`

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,13 @@ from django.conf import settings
 
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
