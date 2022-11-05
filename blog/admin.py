a44fd469c2ab97a5dbb7884cb09c4a73e91cb9de
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-05 22:08:35 +0100

    chore: register the admin for the post model

--- blog/admin.py
+++ blog/admin.py
@@ -12,3 +12,42 @@ class ProfileAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     model = Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        'id',
+        'title',
+        'subtitle',
+        'slug',
+        'publish_date',
+        'published',
+    )
+    list_filter = (
+        'published',
+        'publish_date',
+    )
+    list_editable = (
+        'title',
+        'subtitle',
+        'slug',
+        'publish_date',
+        'published',
+    )
+    search_fields = (
+        'title',
+        'subtitle',
+        'slug',
+        'body',
+    )
+    prepopulated_fields = {
+        'slug': (
+            'title',
+            'subtitle'
+        )
+    }
+    data_hierarchy = 'publish_date'
+    save_on_top = True
