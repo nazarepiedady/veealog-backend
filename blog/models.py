7e2e540047ac84d687ed5e50cad4f681a627cafa
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-05 21:53:49 +0100

    chore: create the model `post`

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    class Meta:
+        ordering = ['-publish_date']
+
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_decription = models.CharField(max_length=150, blank=True)
+    data_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
